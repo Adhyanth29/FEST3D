ebd9b1c180e3a9c97ea0ff67d47f5db56be5ceca
Adhyanth29 <adhyanth.ajay@gmail.com>
2021-02-02 17:15:57 +0530

    Populated H sub. Smoothen sub with 2 variables

--- src/multiphase/clsvof_incomp.f90
+++ src/multiphase/clsvof_incomp.f90
@@ -8,7 +8,7 @@ module clsvof_incomp
    !<
    !   -- Adhyanth Giri Ajay
    !   -- First build
-   
+
 #include "../error.h"
 #include "../debug.h"
 
@@ -16,6 +16,7 @@ module clsvof_incomp
    use gradients, only : compute_gradient_G
    implicit none
    private
+   real(wp), parameter :: pi = 3.14159265
 
 
    contains
@@ -25,6 +26,53 @@ module clsvof_incomp
             implicit none
          end subroutine volume_fraction_advection
 
+         ! ! ! ! subroutine setup_clsvof(control, scheme, flow, dims)
+         ! ! ! !    !< allocate array memory for data communication
+         ! ! ! !    implicit none
+         ! ! ! !    type(controltype), intent(in) :: control
+         ! ! ! !    !< Control parameters
+         ! ! ! !    type(schemetype), intent(in) :: scheme
+         ! ! ! !    !< finite-volume Schemes
+         ! ! ! !    type(flowtype), intent(in) :: flow
+         ! ! ! !    !< Information about fluid flow: freestream-speed, ref-viscosity,etc.
+         ! ! ! !    type(extent), intent(in) :: dims
+         ! ! ! !    !< Extent of the domain:imx,jmx,kmx
+         ! ! ! !    character(len=*), parameter :: errmsg="module: CLSVOF_incomp, subroutine setup"
+         ! ! ! !    !< Error message
+         
+         ! ! ! !    imx = dims%imx
+         ! ! ! !    jmx = dims%jmx
+         ! ! ! !    kmx = dims%kmx
+         ! ! ! !    n_var = control%n_var
+         ! ! ! !    gm = flow%gm
+         ! ! ! !    mu_ref = flow%mu_ref
+         ! ! ! !    Reynolds_number = flow%Reynolds_number
+         ! ! ! !    free_stream_tu = flow%tu_inf
+         ! ! ! !    tk_inf = flow%tk_inf
+         ! ! ! !    tkl_inf = flow%tkl_inf
+         ! ! ! !    tpr = flow%tpr
+         ! ! ! !    pr = flow%pr
+         ! ! ! !    R_gas = flow%R_gas
+         
+         ! ! ! !    call alloc(delQ, 0, imx, 0, jmx, 0, kmx, 1, n_var)
+         ! ! ! !    call alloc(delQstar, 0, imx, 0, jmx, 0, kmx, 1, n_var)
+         
+         ! ! ! !    if(mu_ref==0.0 .or. scheme%turbulence=='none') then
+         ! ! ! !      call alloc(dummy, 0, imx, 0, jmx, 0, kmx)
+         ! ! ! !      dummy = 0.0
+         ! ! ! !    end if
+         ! ! ! !    if(mu_ref==0.0)then
+         ! ! ! !      mmu => dummy
+         ! ! ! !    else
+         ! ! ! !      mmu => mu
+         ! ! ! !    end if
+         ! ! ! !    if(trim(scheme%turbulence)=='none')then
+         ! ! ! !      tmu => dummy
+         ! ! ! !    else
+         ! ! ! !      tmu => mu_t
+         ! ! ! !    end if
+         ! ! ! ! end subroutine setup_clsvof
+         
 
          subroutine interface_recons()
             !< to reconstruct interface with the 4 filling cases
@@ -76,14 +124,79 @@ module clsvof_incomp
             implicit none
          end subroutine dirac_delta
 
-         subroutine heaviside()
+
+         subroutine heaviside(phi, epsilon, cells, dims)
             !< forming heaviside function based on level-set
             implicit none
+            type(extent), intent(in) :: dims
+            !< Extent of domain: imx, jmx, kmx
+            real(wp), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(out) :: H
+            !< Output variable storing the Heaviside values
+            real(wp), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: phi
+            !< New Level-Set function
+            type(celltype), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: cells
+            !< Input cell quantities: cell centers
+            real(wp), intent(in) :: epsilon
+            !< Numerical interface width
+            integer :: i
+            integer :: j
+            integer :: k
+
+            ! To calcualte heaviside function
+            do k = 1:dims%kmx-1
+               do j = 1:dims%jmx-1
+                  do i = 1:dims%imx-1
+                     if (phi(i,j,k) < -1*epsilon) then 
+                        ! when LS is below interface limit
+                        H(i,j,k) = 0
+                     else if (phi(i,j,k) > epsilon) then
+                        ! when LS is above interface limit
+                        H(i,j,k) = 1
+                     else
+                        ! when LS is in the interface band
+                        H(i,j,k) = 0.5*(1 + phi(i,j,k)/epsilon + SIN(pi*phi/epslion))
+                     end if
+                  end do
+               end do
+            end do             
          end subroutine heaviside
 
 
-         subroutine smoothen_G()
-            !< to smoothen a scalar G along the interface between
-            implicit none 
-            !< two fluids
+         subroutine smoothen_G(G1, G2, H, cells, dims)
+            !< to smoothen two scalars over interface
+            !< between two fluids using Heaviside function
+            implicit none
+            type(extent), intent(in) :: dims
+            !< Extent of domain: imx, jmx, kmx
+            real(wp), dimension(-2:dims%imx+2,-2:dims%jmx/2,-2:dims%kmx+2), intent(out) :: G1
+            !< Input variable for scalar 1 over one half of the interface
+            real(wp), dimension(-2:dims%imx+2,dims%jmx/2+1:dims%jmx +2,-2:dims%kmx+2), intent(out) :: G2
+            !< Input variable for scalar 2 over second half of the interface
+            real(wp), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: H
+            !< New Heaviside function
+            type(celltype), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: cells
+            !< Input cell quantities: cell centers
+            real(wp), intent(in) :: epsilon
+            !< Numerical interface width
+            integer :: i
+            integer :: j
+            integer :: k
+
+            ! To Smoothen function
+            do k = 1:dims%kmx-1
+               do j = 1:dims%jmx-1
+                  do i = 1:dims%imx-1
+                     if (phi(i,j,k) < -1*epsilon) then 
+                        ! when LS is below interface limit
+                        H(i,j,k) = 0
+                     else if (phi(i,j,k) > epsilon) then
+                        ! when LS is above interface limit
+                        H(i,j,k) = 1
+                     else
+                        ! when LS is in the interface band
+                        H(i,j,k) = 0.5*(1 + phi(i,j,k)/epsilon + sin(pi*phi/epslion))
+                     end if
+                  end do
+               end do
+            end do
          end subroutine smoothen_G
