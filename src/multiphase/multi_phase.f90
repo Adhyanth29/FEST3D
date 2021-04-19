95f1462fcf4998267f62fa0256c7a5b93b6ceb7d
Adhyanth29 <adhyanth.ajay@gmail.com>
2021-04-19 19:10:18 +0530

    Coupled to update.f90. Fixed perform_multiphase

--- src/multiphase/multi_phase.f90
+++ src/multiphase/multi_phase.f90
@@ -14,7 +14,7 @@ module multi_phase
    !< Public Members
    public :: setup_multiphase_scheme
    public :: perform_multiphase
-   public :: compute_residue
+   public :: update_state
 
    contains
          subroutine setup_multiphase_scheme(F_surface, control, dims)
@@ -41,13 +41,13 @@ module multi_phase
          end subroutine setup_multiphase_scheme
 
 
-         subroutine perform_multiphase(F_surface, delta_t, Ifaces, Jfaces, Kfaces, scheme, flow, dims, qp, nodes)
+         subroutine perform_multiphase(F_surface, control, Ifaces, Jfaces, Kfaces, scheme, flow, dims, qp, nodes)
             !< Authorises the type of multiphase operation
             implicit none
             type(schemetype), intent(in) :: scheme
             !< finite-volume Schemes
-            !type(flowtype), intent(in) :: flow
-            !< Information about fluid flow: freestream-speed, ref-viscosity,etc.
+            type(controltype), intent(in) :: control
+            !< Control parameters
             type(extent), intent(in) :: dims
             !< extent of the 3D domain
             real(wp), dimension(:, :, :, :), intent(inout) :: F_surface
@@ -57,6 +57,9 @@ module multi_phase
             type(celltype), dimension(-2:dims%imx+2,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: cells
             !< Stores cell parameter: volume
             real(wp), dimension(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2, 1:dims%n_var), intent(inout), target :: qp
+            !< Stores state parameters
+            type(flowtype), intent(in) :: flow
+            !< Information about fluid flow: freestream-speed, ref-viscosity,etc.
             type(facetype), dimension(-2:dims%imx+3,-2:dims%jmx+2,-2:dims%kmx+2), intent(in) :: Ifaces
             !< Store face quantites for I faces 
             type(facetype), dimension(-2:dims%imx+2,-2:dims%jmx+3,-2:dims%kmx+2), intent(in) :: Jfaces
@@ -65,15 +68,15 @@ module multi_phase
             !< Store face quantites for K faces
             real(wp) , dimension(1:dims%imx-1, 1:dims%jmx-1, 1:dims%kmx-1), intent(in) :: delta_t
             !< Local time increment value at each cell center
-            real(wp) :: sigma, epsilon
+            
 
             select case (trim(scheme%multiphase))
                 case ("clsvof")
                   !< For the incompressible Coupled Level-Set Volume of Fluid case
                   sigma = flow%sigma
                   epsilon = flow%epsilon
-                  call perform_clsvof_incomp(dims, nodes, cells, Ifaces, Jfaces, Kfaces, sigma, epsilon, F_surface, delta_t)
-                  !call compute_fluxes_van_leer(F_x,F_y,F_z,x_qp_left,x_qp_right,y_qp_left,y_qp_right,z_qp_left,z_qp_right,Ifaces,Jfaces,Kfaces,flow,bc,dims)
+                  call perform_clsvof_incomp(qp, Ifaces, Jfaces, Kfaces, del_t, flow, nodes, cells, dims, F_surface)
+                  
                 case ("clsvof_c")
                   !< For the compressible Coupled Level-Set Volume of Fluid case
                   !call compute_fluxes_ausm(F_x,F_y,F_z,x_qp_left,x_qp_right,y_qp_left,y_qp_right,z_qp_left,!z_qp_right,Ifaces,Jfaces,Kfaces,flow,bc,dims)
@@ -92,34 +95,51 @@ module multi_phase
             
          end subroutine perform_multiphase
 
-         subroutine compute_residue(residue,F_x,F_y,F_z,dims)
+        !  subroutine update_qp(qp,flow,dims)
             
-            implicit none
-            type(extent), intent(in) :: dims
-            !< extent of the 3D domain
-            real(wp), dimension(:, :, :, :), intent(out)  :: residue
-            !< Store residue at each cell-center
-            real(wp), dimension(:, :, :, :), intent(in) :: F_x
-            !< Store fluxes throught the I faces
-            real(wp), dimension(:, :, :, :), intent(in) :: F_y
-            !< Store fluxes throught the J faces
-            real(wp), dimension(:, :, :, :), intent(in) :: F_z
-            !< Store fluxes throught the K faces
+        !     implicit none
+        !     type(extent), intent(in) :: dims
+        !     !< extent of the 3D domain
+        !     real(wp), dimension(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2, 1:dims%n_var), intent(inout), target :: qp
+        !     !< Stores state parameters
+        !     type(flowtype), intent(in) :: flow
+        !     !< Information about fluid flow: freestream-speed, ref-viscosity,etc.
+        !     real(wp), dimension(:,:,:),allocatable :: density
+        !     !< Pointer for density slice in qp
+        !     real(wp), dimension(:,:,:),allocatable :: x_speed
+        !     !< Pointer for U velocity slice in qp
+        !     real(wp), dimension(:,:,:),allocatable :: y_speed
+        !     !< Pointer for V velocity slice in qp
+        !     real(wp), dimension(:,:,:),allocatable :: z_speed
+        !     !< Pointer for W velocity slice in qp
+        !     real(wp), dimension(:,:,:),allocatable :: vof
+        !     !< Pointer for Volume Fraction slice in qp
+        !     ! real(wp), dimension(:, :, :, :), intent(in) :: F_x
+        !     ! !< Store fluxes throught the I faces
+        !     ! real(wp), dimension(:, :, :, :), intent(in) :: F_y
+        !     ! !< Store fluxes throught the J faces
+        !     ! real(wp), dimension(:, :, :, :), intent(in) :: F_z
+        !     ! !< Store fluxes throught the K faces
             
-            integer :: i, j, k, l
+        !     integer :: i, j, k, l
 
-            DebugCall('compute_residue')
+        !     ! Pointer allocation
+        !     density(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 1)
+        !     !!!density_2(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 9)
+        !     x_speed(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 2)
+        !     y_speed(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 3)
+        !     z_speed(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 4)
+        !     vof(-2:dims%imx+2, -2:dims%jmx+2, -2:dims%kmx+2) => qp(:, :, :, 10)
+        !     DebugCall('update_qp')
 
-            do l = 1, dims%n_var
-             do k = 1, dims%kmx - 1
-              do j = 1, dims%jmx - 1
-               do i = 1, dims%imx - 1
-               ! residue(i, j, k, l) = (F_x(i+1, j, k, l) - F_x(i, j, k, l)) &
-               !                     + (F_y(i, j+1, k, l) - F_y(i, j, k, l)) &
-               !                     + (F_z(i, j, k+1, l) - F_z(i, j, k, l))
-               end do
-              end do
-             end do
-            end do
+        !     do l = 1, dims%n_var
+        !      do k = 1, dims%kmx - 1
+        !       do j = 1, dims%jmx - 1
+        !        do i = 1, dims%imx - 1
+
+        !        end do
+        !       end do
+        !      end do
+        !     end do
         
-        end subroutine compute_residue
+        ! end subroutine update_qp
